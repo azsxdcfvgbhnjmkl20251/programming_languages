1c6ed80f015277546f373ee7f614e2337ead1fcc
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2025-11-25 08:14:43 +0530

    C

---
+++ rust/_3_server.rs
@@ -0,0 +1,47 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    //let contents = fs::read_to_string("_3_index.html").unwrap(); 
+    let contents = "
+<html>
+<head>
+    <meta charset=\"utf-8\">
+    <title>Hello!</title>
+</head>
+
+<body>
+    <h1>Hello!</h1>
+    <p>Hi from Rust</p>
+    <script>alert(2)</script>
+</body>
+
+</html>";
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
---
+++ rust/readme.txt
@@ -0,0 +1,7 @@
+Steps to install and run Rust:
+
+- brew install rust
+- rustc _1_hello.rs
+- ./_1_hello
+
+- rustc _3_server.rs && ./_3_server
